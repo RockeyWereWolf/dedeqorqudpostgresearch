7b43961027e23e82b2b56c5de035dee04ff6164a
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-03-09 21:04:24 +0400

    Update kitabe-dede-qorqud.sql
    add a test contents to 1st book

--- kitabe-dede-qorqud.sql
+++ kitabe-dede-qorqud.sql
@@ -4,7 +4,7 @@ CREATE TABLE books (
   main_character TEXT NOT NULL,
   prologue TEXT NOT NULL
 );
-
+/*
 CREATE TABLE episodes (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
@@ -21,3 +21,13 @@ CREATE TABLE characters (
   FOREIGN KEY (book_id) REFERENCES books(id),
   FOREIGN KEY (episode_id) REFERENCES episodes(id)
 );
+*/
+INSERT INTO books (title, main_character, prolugue)
+VALUES ('Boghach Khan Son of Dirse Khan','Boghach Khan','One day Bayindir Khan son of Kam Ghan rose up from his
+place. He had his striped parasol set up on the earth’s face, his
+many-coloured pavilion reared up to the face of the sky. In a
+thousand places silken rugs were spread. Once a year the Khan
+of Khans, Bayindir Khan, used to make a feast and entertain
+the Oghuz nobles. This year again he made a feast and had his
+men slaughter of horses the stallions, of camels the males, of
+sheep the rams');
