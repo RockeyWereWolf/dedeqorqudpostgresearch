60a04d3725b4191fcbffe5724e3393f7a795912f
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-03-09 21:41:31 +0400

    Update main.go

--- main.go
+++ main.go
@@ -13,9 +13,7 @@ import (
     log "github.com/sirupsen/logrus"  
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World!</h1>"))
-}
+var db *sql.DB
 
 func main() {
     // Get the database connection parameters from environment variables
@@ -55,4 +53,68 @@ func main() {
     if err != nil {
         log.Fatal(err) 
     }
+    // Define the HTTP handlers
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/search", searchHandler)
+
+    // Start the HTTP server
+    err = http.ListenAndServe(":8080", nil)
+    if err != nil {
+	panic(err)
+    }
+}
+//Initial html page layout
+func homePage(w http.ResponseWriter, r *http.Request) {
+    // Add the HTML form to get the user's search term
+    fmt.Fprintf(w, "<form method='POST' action='/search'>")
+    fmt.Fprintf(w, "<input type='text' name='searchTerm'>")
+    fmt.Fprintf(w, "<input type='submit' value='Search'>")
+    fmt.Fprintf(w, "</form>")
+}
+//Executing search results
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+    // Get the search term from the form data
+    searchTerm := r.FormValue("searchTerm")
+
+    // Perform the full text search
+    results, err := performFullTextSearch(searchTerm)
+    if err != nil {
+        http.Error(w, "Error performing full text search", http.StatusInternalServerError)
+        return
+    }
+
+    // Display the results on the web page
+    fmt.Fprintf(w, "<h1>Search Results</h1>")
+    for _, result := range results {
+        fmt.Fprintf(w, "<p>%s</p>", result)
+    }
+}
+//Performing PostgreSQL full text search on the response
+func performFullTextSearch(searchTerm string) ([]string, error) {
+    // Define the search query
+    query := fmt.Sprintf("SELECT text FROM documents WHERE to_tsvector('english', text) @@ to_tsquery('english', '%s')", searchTerm)
+
+    // Execute the search query
+    rows, err := db.Query(query)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    // Collect the search results
+    var results []string
+    for rows.Next() {
+        var text string
+        err = rows.Scan(&text)
+        if err != nil {
+            return nil, err
+        }
+        results = append(results, text)
+    }
+    err = rows.Err()
+    if err != nil {
+        return nil, err
+    }
+
+    return results, nil
 }
