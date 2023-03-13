4dc186c97fd541990a6b2d2bc28bab44397cdbdd
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-03-13 23:23:32 +0400

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
     _ "github.com/lib/pq"
     log "github.com/sirupsen/logrus"  
 )
-var db *sql.DB
+
 func main() {
     // Get the database connection parameters from environment variables
     host := os.Getenv("PGHOST")
@@ -42,9 +42,9 @@ func main() {
     if err != nil {
         log.Fatal(err) 
     } 
-    // Define the HTTP handlers
-	http.HandleFunc("/", homePage)
-	//http.HandleFunc("/search", searchHandler)
+     // Define the HTTP handlers
+    http.HandleFunc("/", homePageHandler(db))
+    http.HandleFunc("/search", searchHandler(db)
     // Start the HTTP server
     err = http.ListenAndServe(":8080", nil)
     if err != nil {
@@ -53,31 +53,35 @@ func main() {
 }
 //Read and execute sql files
 //Initial html page layout
-func homePage(w http.ResponseWriter, r *http.Request) {
-    // Add the HTML form to get the user's search term
-    fmt.Fprintf(w, "<form method='POST' action='/search'>")
-    fmt.Fprintf(w, "<input type='text' name='searchTerm'>")
-    fmt.Fprintf(w, "<input type='submit' value='Search'>")
-    fmt.Fprintf(w, "</form>")
+func homePageHandler(db *sql.DB) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        // Add the HTML form to get the user's search term
+        fmt.Fprintf(w, "<form method='POST' action='/search'>")
+        fmt.Fprintf(w, "<input type='text' name='searchTerm'>")
+        fmt.Fprintf(w, "<input type='submit' value='Search'>")
+        fmt.Fprintf(w, "</form>")
+    }
 }
 //Executing search results
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-    // Get the search term from the form data
-    searchTerm := r.FormValue("searchTerm")
-    // Perform the full text search
-    results, err := performFullTextSearch(searchTerm)
-    if err != nil {
-        http.Error(w, "Error performing full text search", http.StatusInternalServerError)
-        return
-    }
-    // Display the results on the web page
-    fmt.Fprintf(w, "<h1>Search Results</h1>")
-    for _, result := range results {
-        fmt.Fprintf(w, "<p>%s</p>", result)
+func searchHandler(db *sql.DB) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        // Get the search term from the form data
+        searchTerm := r.FormValue("searchTerm")
+        // Perform the full text search
+        results, err := performFullTextSearch(db, searchTerm)
+        if err != nil {
+            http.Error(w, "Error performing full text search", http.StatusInternalServerError)
+            return
+        }
+        // Display the results on the web page
+        fmt.Fprintf(w, "<h1>Search Results</h1>")
+        for _, result := range results {
+            fmt.Fprintf(w, "<p>%s</p>", result)
+        }
     }
 }
 //Performing PostgreSQL full text search on the response
-func performFullTextSearch(searchTerm string) ([]string, error) {
+func performFullTextSearch(db *sql.DB, searchTerm string) ([]string, error) {
     // Define the search query
     query := fmt.Sprintf("SELECT text FROM documents WHERE to_tsvector('english', text) @@ to_tsquery('english', '%s')", searchTerm)
     // Execute the search query
