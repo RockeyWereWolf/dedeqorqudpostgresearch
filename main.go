a1ed38b7b4f0f0937237baee2538ad2a8c1e3714
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-03-14 23:23:05 +0400

    Update main.go

--- main.go
+++ main.go
@@ -77,78 +77,78 @@ func main() {
 		}
 	
 		// Execute the full-text search query
-rows, err := db.Query(`
-SELECT id, title, main_character, content, ts_headline(content, q, 'StartSel = <mark>, StopSel = </mark>, MaxWords = 100, MinWords = 10, ShortWord = 3, HighlightAll = true') AS snippet
-FROM books, to_tsquery($1) AS q
-WHERE to_tsvector('english', content) @@ q
-`, query)
-if err != nil {
-http.Error(w, "Failed to execute query", http.StatusInternalServerError)
-log.Error(err)
-return
-}
-defer rows.Close()
+        rows, err := db.Query(`
+            SELECT id, title, main_character, content, ts_headline(content, q, 'StartSel = <mark>, StopSel = </mark>, MaxWords = 100, MinWords = 10, ShortWord = 3, HighlightAll = true') AS snippet
+            FROM books, to_tsquery($1) AS q
+            WHERE to_tsvector('english', content) @@ q
+        `, query)
+        if err != nil {
+            http.Error(w, "Failed to execute query", http.StatusInternalServerError)
+            log.Error(err)
+        return
+        }
+        defer rows.Close()
 
-// Display the search results in an HTML page
-fmt.Fprintf(w, `
-<html>
-	<head><title>Search Results</title></head>
-	<body>
-		<h1>Search Results</h1>
-		<p>Showing results for: %q</p>
-`, query)
-for rows.Next() {
-var id int
-var title, main_character, content, snippet string
-if err := rows.Scan(&id, &title, &main_character, &content, &snippet); err != nil {
-	http.Error(w, "Failed to scan row", http.StatusInternalServerError)
-	log.Error(err)
-	return
-}
+        // Display the search results in an HTML page
+        fmt.Fprintf(w, `
+        <html>
+	        <head><title>Search Results</title></head>
+	        <body>
+		        <h1>Search Results</h1>
+		        <p>Showing results for: %q</p>
+            `, query)
+            for rows.Next() {
+                var id int
+                var title, main_character, content, snippet string
+                    if err := rows.Scan(&id, &title, &main_character, &content, &snippet); err != nil {
+	                http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+	            log.Error(err)
+	        return
+        }
 
-// Count the number of sentences in the content
-sentences := strings.Split(content, ".")
-numSentences := len(sentences)
+        // Count the number of sentences in the content
+        sentences := strings.Split(content, ".")
+        numSentences := len(sentences)
 
-// Find the sentences containing the search query
-var matches []int
-for i, sentence := range sentences {
-	if strings.Contains(sentence, query) {
-		matches = append(matches, i)
-	}
-}
+        // Find the sentences containing the search query
+        var matches []int
+        for i, sentence := range sentences {
+	        if strings.Contains(sentence, query) {
+		    matches = append(matches, i)
+	        }
+        }
 
-// Display the search result for the current book
-fmt.Fprintf(w, `
-	<div>
-		<h3>Book %d</h3>
-		<p><em>%s</em></p>
-		<p>Found in %d sentence(s)</p>
-`, id, title, len(matches))
-for _, match := range matches {
-	sentence := sentences[match]
-	// Highlight the search query in the sentence
-	sentence = strings.ReplaceAll(sentence, query, "<mark>"+query+"</mark>")
-	fmt.Fprintf(w, `
-		<p>%d. %s</p>
-	`, match+1, sentence)
-}
-fmt.Fprintf(w, `
-	<p>Out of %d sentence(s)</p>
-	<p>%s</p>
-</div>
-`, numSentences, snippet)
-}
-if err := rows.Err(); err != nil {
-http.Error(w, "Failed to iterate over results", http.StatusInternalServerError)
-log.Error(err)
-return
-}
-fmt.Fprintf(w, `
-</body>
-</html>
-`)
-})
+        // Display the search result for the current book
+        fmt.Fprintf(w, `
+	        <div>
+		        <h3>Book %d</h3>
+		            <p><em>%s</em></p>
+		            <p>Found in %d sentence(s)</p>
+                `, id, title, len(matches))
+            for _, match := range matches {
+	            sentence := sentences[match]
+	            // Highlight the search query in the sentence
+	            sentence = strings.ReplaceAll(sentence, query, "<mark>"+query+"</mark>")
+	            fmt.Fprintf(w, `
+		            <p>%d. %s</p>
+	            `, match+1, sentence)
+            }
+            fmt.Fprintf(w, `
+	            <p>Out of %d sentence(s)</p>
+	            <p>%s</p>
+            </div>
+        `, numSentences, snippet)
+        }
+        if err := rows.Err(); err != nil {
+            http.Error(w, "Failed to iterate over results", http.StatusInternalServerError)
+            log.Error(err)
+        return
+        }
+        fmt.Fprintf(w, `
+                </body>
+            </html>
+        `)
+    })
 	// Start the HTTP server and listen for incoming requests
 	addr := ":8080"
 	log.Infof("Starting server at %s", addr)
