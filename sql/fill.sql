71f2ccf6bba4e6f821aa67e09db11768a7c9f44a
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-03-16 10:28:25 +0400

    Delete sql directory

--- sql/fill.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO books (title, main_character, prolugue)
-VALUES ('Boghach Khan Son of Dirse Khan','Boghach Khan','One day Bayindir Khan son of Kam Ghan rose up from his
-place. He had his striped parasol set up on the earth’s face, his
-many-coloured pavilion reared up to the face of the sky. In a
-thousand places silken rugs were spread. Once a year the Khan
-of Khans, Bayindir Khan, used to make a feast and entertain
-the Oghuz nobles. This year again he made a feast and had his
-men slaughter of horses the stallions, of camels the males, of
-sheep the rams');
--- sql/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  main_character TEXT NOT NULL,
-  prologue TEXT NOT NULL
-);
